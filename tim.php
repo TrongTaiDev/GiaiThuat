99d7290c256f7cd5d02527a0035b69b710cc60b1
Nguyen Trong Tai <trongtai.dev@gmail.com>
2018-03-26 00:07:48 +0700

    Add files via upload

---
+++ tim.php
@@ -0,0 +1,6 @@
+<form class="formContainer" method="get" action="ketquatim.php">
+	<h3 class="center">TÌM KIẾM SINH VIÊN</h3>
+	<label for="thongtin">Thông tin tìm kiếm</label>
+	<input type="text" name="thongtin" class="input"/>
+	<input type="submit" name="timkiem" class="submit" value="Tìm kiếm"/>
+</form>
